eb26e35e8f3c63eb5e315293500049e23aecab03
rafaelthz <rafaelthomaz@uel.br>
2021-06-24 00:20:24 -0300

    Room and questions flow

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,21 @@
+import copyImg from '../assets/images/copy.svg';
+
+import '../styles/room-code.scss';
+
+type RoomCodeProps = {
+  code: string
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+  }
+  return (
+    <button className="room-code" onClick={copyRoomCodeToClipboard} title="Copiar código da sala.">
+      <div>
+        <img src={copyImg} alt="Copiar código da sala." />
+      </div>
+      <span>{props.code}</span>
+    </button>
+  );
+}
