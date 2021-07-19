7539b99b935adf2b2f7f5029428b121bb38db368
rafaelthz <rafaelthomaz@uel.br>
2021-07-19 16:53:29 -0300

    Add toast notifications

--- src/components/RoomCode/index.tsx
+++ src/components/RoomCode/index.tsx
@@ -1,4 +1,6 @@
 import copyImg from '../../assets/images/copy.svg';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import './styles.scss';
 
@@ -9,13 +11,27 @@ type RoomCodeProps = {
 export function RoomCode(props: RoomCodeProps) {
   function copyRoomCodeToClipboard() {
     navigator.clipboard.writeText(props.code);
+    toast.info("Código copiado!");
   }
   return (
-    <button className="room-code" onClick={copyRoomCodeToClipboard} title="Copiar código da sala.">
-      <div>
-        <img src={copyImg} alt="Copiar código da sala." />
-      </div>
-      <span>{props.code}</span>
-    </button>
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <button className="room-code" onClick={copyRoomCodeToClipboard} title="Copiar código da sala.">
+        <div>
+          <img src={copyImg} alt="Copiar código da sala." />
+        </div>
+        <span>{props.code}</span>
+      </button>
+    </>
   );
 }
